6b58aa5aa8130bb4b5de1c0f1ad8d0c784e470ab
Jaakko Korhonen <jaakko.korhonen@nordicsemi.no>
2022-09-12 09:03:23 +0300

    Add variables to setup.py.

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 """This module defines setup procedure.
 """
+import os
 from setuptools import setup, find_packages
 
+pkg_name = "template-python-project"
+
+# Get version
+exec(open(os.path.join(pkg_name, "version.py")).read())
+
 setup(
-    name="template-python-project",
-    version="0.1.0",
+    name=pkg_name,
+    version=__version__,
     url="https://github.com/korjaa/template-python-project",
     author="Jaakko Korhonen",
     author_email="user@email.com",
@@ -12,7 +18,7 @@ setup(
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "template-python-project=template_python_project.cli:main"
+            f"{pkg_name}={pkg_name}.cli:main"
         ]
     },
     install_requires=[
